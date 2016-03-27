e62a9bc4100cab592aceff3f18cd31c6e9651d0e
jessicalh <jessicalh@hotmail.com>
2016-03-26 22:07:14 -0700

    Squaring away setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name = "ekmmeters",
+    version = "0.1.0",
+    license='MIT',
+    description='Python API for V3 and V4 EKM Omnimeters',
+    author = 'EKM Metering',
+    author_email = "info@ekmmetering.com",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    py_modules=['ekmmeters'],
+    install_requires=[
+          'importlib==1.0.3',
+          'OrderedDict==1.1',
+          'pyserial==3.0.1',
+      ]
+)
