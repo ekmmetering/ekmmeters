17ff6416cad58358edda4c91bd910265603c5142
jessicalh <jessicalh@hotmail.com>
2016-03-26 23:27:23 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     # Override default css to get a larger width for local build                 
     def setup(app):                                                              
         #app.add_javascript("custom.js")                                         
-        app.add_stylesheet('theme_overrides.css')                                
+        app.add_stylesheet('custom.css')                                
 else:                                                                            
     # Override default css to get a larger width for ReadTheDoc build            
     html_context = {                                                             
