368f8e9c95b8ddec4c22068ab9a699df46c99c32
jessicalh <jessicalh@hotmail.com>
2016-04-05 08:06:28 -0700

    ver updates

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name = "ekmmeters",
-    version = "0.2.0",
+    version = "0.2.2",
     license='MIT',
     description='Python API for V3 and V4 EKM Omnimeters',
     author = 'EKM Metering',
     author_email = "info@ekmmetering.com",
     url = 'https://github.com/jessicalh/ekmmeters',
-    download_url = 'https://github.com/jessicalh/ekmmeters/tarball/0.2.0',
+    download_url = 'https://github.com/jessicalh/ekmmeters/tarball/0.2.2',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
